860c56ce3fd3d78e4ea49e3d277bf8e1b5a51a65
1C0D <63018905+1C0D@users.noreply.github.com>
2020-10-09 15:35:00 +0200

    correct UVS bool added

--- remove_doubles_plus v1_4_1.py
+++ remove_doubles_plus v1_4_1.py
@@ -7,75 +7,87 @@ import sys
 bl_info = {
     "name": "remove doubles plus",
     "author": "1C0D",
-    "version": (1, 4, 0),
+    "version": (1, 4, 1),
     "blender": (2, 81, 0),
     "category": "Mesh"}
-    
+
+'''todo:option UVs or not
+add move to cursor (offset)to menu?'''
+
 class Multi_OT_Merge(bpy.types.Operator):
     """merging 2 lists of vertices"""
     bl_idname = "multi.merge"
     bl_label = "Multi Merge"
     bl_description = 'merge 2 separated rows of same number of vertices at once'
     bl_options = {"UNDO", "REGISTER"}
-    
+
     def update_center(self, context):
         if self.merge_center:
-            self.merge_first = False        
-            self.merge_last = False  
-            
-    def update_last(self,context):
-        if self.merge_last:
-            self.merge_first = False        
-            self.merge_center = False  
-            
-    def update_first(self,context):
-        if self.merge_first:
-            self.merge_last = False        
-            self.merge_center = False        
+            self.merge_first = False
+            self.merge_last = False
 
-    merge_center: BoolProperty(name="multi merge centre", default=True, update=update_center)
-    merge_last: BoolProperty(name="multi merge last", default=False, update=update_last)
-    merge_first: BoolProperty(name="multi merge first", default=False, update=update_first)
-    
+    def update_last(self, context):
+        if self.merge_last:
+            self.merge_first = False
+            self.merge_center = False
+
+    def update_first(self, context):
+        if self.merge_first:
+            self.merge_last = False
+            self.merge_center = False
+
+    merge_center: BoolProperty(
+        name="multi merge centre", default=True, update=update_center)
+    merge_last: BoolProperty(name="multi merge last",
+                             default=False, update=update_last)
+    merge_first: BoolProperty(name="multi merge first",
+                              default=False, update=update_first)
+    keep_UVs: BoolProperty(name="Correct Uvs", default=True)
+
     def execute(self, context):
-    
+
         obj = bpy.context.active_object
         mesh = obj.data
         bm = bmesh.from_edit_mesh(mesh)
-        bm.verts.ensure_lookup_table()       
-        history=bm.select_history       
+        bm.verts.ensure_lookup_table()
+        history = bm.select_history
 
         def deselect():
-            for f in bm.edges:f.select = False
-            for e in bm.edges:e.select = False
-            for v in bm.verts:v.select = False
+            for f in bm.edges:
+                f.select = False
+            for e in bm.edges:
+                e.select = False
+            for v in bm.verts:
+                v.select = False
             bm.select_flush(False)
 
-        def Merge(v1,v2):            
-            v1.select =True
-            v2.select =True
+        def Merge(v1, v2):
+            v1.select = True
+            v2.select = True
+            
+            UVs=self.keep_UVs
             if self.merge_center:
-                bpy.ops.mesh.merge(type='CENTER', uvs=True)
+                bpy.ops.mesh.merge(type='CENTER', uvs=UVs)
             if self.merge_last:
                 history.add(v2)
-                bpy.ops.mesh.merge(type='LAST', uvs=True) #'INVOKE_DEFAULT'
+                bpy.ops.mesh.merge(type='LAST', uvs=UVs)  # 'INVOKE_DEFAULT'
             if self.merge_first:
                 history.add(v1)
-                bpy.ops.mesh.merge(type='FIRST', uvs=True)
+                bpy.ops.mesh.merge(type='FIRST', uvs=UVs)
             deselect()
-        
-        sel=[v for v in bm.verts if v.select]
-        
-        if len(sel)!=4:
+
+        sel = [v for v in bm.verts if v.select]
+
+        if len(sel) != 4:
             self.report({'ERROR'}, "Select 4 vertices with Shift")
             return {'CANCELLED'}
-            
+
         for item in history:
             if not isinstance(item, bmesh.types.BMVert):
                 self.report({'ERROR'}, "Select Vertices only")
                 return {'CANCELLED'}
-        
-        try:        
+
+        try:
             V0 = history[-4]
             V1 = history[-3]
             V2 = history[-2]
@@ -84,19 +96,19 @@ class Multi_OT_Merge(bpy.types.Operator):
             self.report({'ERROR'}, "Select 4 vertices with Shift")
             return {'CANCELLED'}
 
-        def path(v1,v2):
-            v1.select=True
-            v2.select=True
+        def path(v1, v2):
+            v1.select = True
+            v2.select = True
             bpy.ops.mesh.shortest_path_select()
-            path=[v for v in bm.verts if v.select]
-            path_copy=path.copy()
+            path = [v for v in bm.verts if v.select]
+            path_copy = path.copy()
 
-            sorted_path=[v1]
+            sorted_path = [v1]
 
-            while len(path_copy)>1:
+            while len(path_copy) > 1:
                 v = sorted_path[-1]
                 for e in v.link_edges:
-                    if e.other_vert(v) in path_copy: 
+                    if e.other_vert(v) in path_copy:
                         path_copy.remove(v)
                         sorted_path.append(e.other_vert(v))
             if v2 not in sorted_path:
@@ -104,15 +116,15 @@ class Multi_OT_Merge(bpy.types.Operator):
 
             return sorted_path
 
-        deselect()        
-        path1 = path(V0,V1)
         deselect()
-        path2 = path(V2,V3)
-        deselect()        
-        [Merge(v1,v2) for v1,v2 in zip(path1,path2)]
+        path1 = path(V0, V1)
+        deselect()
+        path2 = path(V2, V3)
+        deselect()
+        [Merge(v1, v2) for v1, v2 in zip(path1, path2)]
         bm.normal_update()
         bmesh.update_edit_mesh(mesh)
-        
+
         return {'FINISHED'}
 
 
@@ -140,7 +152,7 @@ class Remove_OT_doubles_plus(bpy.types.Operator):
     rmv_loose_faces: BoolProperty(name="loose faces", default=False)
 
     def clean_geometry(self, bm):
-        
+
         if self.recalc_normals:
             bmesh.ops.recalc_face_normals(bm, faces=bm.faces)
 
@@ -180,7 +192,6 @@ class Remove_OT_doubles_plus(bpy.types.Operator):
                                             .angle(v.link_edges[1].other_vert(v).co - v.co)) < 181)]
             bmesh.ops.dissolve_verts(bm, verts=straight_edged)
 
-
     def execute(self, context):
 
         for ob in context.selected_objects:
@@ -188,14 +199,15 @@ class Remove_OT_doubles_plus(bpy.types.Operator):
                 bm = bmesh.from_edit_mesh(ob.data)
                 bm.normal_update()
                 bm.verts.ensure_lookup_table()
-                verts=len(bm.verts[:])
-                edges=len(bm.edges[:])
-                faces=len(bm.faces[:])
+                verts = len(bm.verts[:])
+                edges = len(bm.edges[:])
+                faces = len(bm.faces[:])
                 self.clean_geometry(bm)
-                rmv_verts= verts-len(bm.verts)
-                rmv_edges= edges-len(bm.edges)
-                rmv_faces= faces-len(bm.faces)
-                self.report({'WARNING'}, f"-[Vert:{rmv_verts}, Edg:{rmv_edges}, Fc:{rmv_faces}]")
+                rmv_verts = verts-len(bm.verts)
+                rmv_edges = edges-len(bm.edges)
+                rmv_faces = faces-len(bm.faces)
+                self.report(
+                    {'WARNING'}, f"-[Vert:{rmv_verts}, Edg:{rmv_edges}, Fc:{rmv_faces}]")
                 bmesh.update_edit_mesh(ob.data)
 
         return {'FINISHED'}
@@ -204,6 +216,7 @@ class Remove_OT_doubles_plus(bpy.types.Operator):
 def doubles_plus(self, context):
 
     self.layout.operator("remove.doubles_plus")
+
 
 def multi_merge(self, context):
 
